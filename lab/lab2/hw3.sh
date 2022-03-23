8063d3c84a1dfca5706dc0ff7e2eded9abcccebe
ubuntuvm <sunjcihen21@126.com>
2022-03-23 22:18:23 +0800

    updated lab2 code

--- lab/lab2/hw3.sh
+++ lab/lab2/hw3.sh
@@ -1,3 +1,7 @@
 echo "adding flow rules.."
-sudo ovs-ofctl add-flow s1 "in_port=3 actions=output:1,2"
+sudo ovs-ofctl add-flow s1 "in_port=1 actions=output:2,4"
+sudo ovs-ofctl add-flow s1 "in_port=2 actions=output:1,4"
 sudo ovs-ofctl add-flow s1 "in_port=4 actions=output:1,2"
+sudo ovs-ofctl add-flow s2 "in_port=2 actions=output:3,4"
+sudo ovs-ofctl add-flow s2 "in_port=3 actions=output:2,4"
+sudo ovs-ofctl add-flow s2 "in_port=4 actions=output:2,3"
